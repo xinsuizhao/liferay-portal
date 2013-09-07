2810c2c831403e404ab2eaa174e01687faf7cc1f
Brian Chan <brian.chan@liferay.com>
2013-09-06 20:22:44 -0700

    EE specifics

--- portal-service/src/com/liferay/portal/kernel/util/ReleaseInfo.java
+++ portal-service/src/com/liferay/portal/kernel/util/ReleaseInfo.java
@@ -136,7 +136,7 @@ public class ReleaseInfo {
 		return _VERSION;
 	}
 
-	private static final String _BUILD = "6200";
+	private static final String _BUILD = "6210";
 
 	private static final int _BUILD_NUMBER = GetterUtil.getInteger(_BUILD);
 
@@ -156,9 +156,9 @@ public class ReleaseInfo {
 
 	private static final String _VENDOR = "Liferay, Inc.";
 
-	private static final String _VERSION = "6.2.0";
+	private static final String _VERSION = "6.2.10";
 
-	private static final String _VERSION_DISPLAY_NAME = "6.2.0 CE B3";
+	private static final String _VERSION_DISPLAY_NAME = "6.2.10 EE";
 
 	private static String _releaseInfo;
 	private static String _serverInfo;
