cb3c8102e999331869a1aa2293fc4ed912a80039
Julio Camarero <julio.camarero@liferay.com>
2013-11-06 14:47:36 -0800

    LPS-42068 canonical url should be always the same (independently of the domain you use to access the page)

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -7120,7 +7120,7 @@ public class PortalImpl implements Portal {
 	protected String getCanonicalDomain(
 		boolean canonicalURL, String virtualHostname, String portalDomain) {
 
-		if (!canonicalURL || Validator.isBlank(portalDomain)) {
+		if (canonicalURL || Validator.isBlank(portalDomain)) {
 			return virtualHostname;
 		}
 
