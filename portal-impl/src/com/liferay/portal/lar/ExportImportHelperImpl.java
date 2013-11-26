35cce17dcf2110be60caf25cf67631be8bee4dc1
Eduardo Garcia <eduardo.garcia@liferay.com>
2013-11-26 18:34:09 +0100

    LPS-42288 Exclude portlets that are not dataSiteLevel from import layouts content list

--- portal-impl/src/com/liferay/portal/lar/ExportImportHelperImpl.java
+++ portal-impl/src/com/liferay/portal/lar/ExportImportHelperImpl.java
@@ -1903,6 +1903,7 @@ public class ExportImportHelperImpl implements ExportImportHelper {
 
 				if (!(portletDataHandler instanceof
 						DefaultConfigurationPortletDataHandler) &&
+					portletDataHandler.isDataSiteLevel() &&
 					GetterUtil.getBoolean(
 						element.attributeValue("portlet-data"))) {
 
