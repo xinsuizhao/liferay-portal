1da42e04084f24505e173230f3e85f539ecff6f6
Brian Chan <brian.chan@liferay.com>
2014-01-17 14:43:43 -0800

    LPS-43599

--- portal-impl/src/com/liferay/portal/tools/PluginsEnvironmentBuilder.java
+++ portal-impl/src/com/liferay/portal/tools/PluginsEnvironmentBuilder.java
@@ -639,7 +639,7 @@ public class PluginsEnvironmentBuilder {
 		_fileUtil.write(projectFile, sb.toString());
 	}
 
-	private static final String _BRANCH = "master";
+	private static final String _BRANCH = "6.2.x";
 
 	private static final String[] _SOURCE_DIR_NAMES = new String[] {
 		"docroot/WEB-INF/ext-impl/src", "docroot/WEB-INF/ext-service/src",
