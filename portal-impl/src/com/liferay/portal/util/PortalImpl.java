440487c0e5087cb8318071906f8af0a989bb686b
sherryre <sherry.yang@liferay.com>
2014-01-22 17:51:13 -0800

    LPS-43510 Source format

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -7134,7 +7134,7 @@ public class PortalImpl implements Portal {
 	}
 
 	protected String getCanonicalDomain(
-		boolean canonicalURL, String virtualHostname, String portalDomain) {
+		String virtualHostname, String portalDomain) {
 
 		if (Validator.isBlank(portalDomain) ||
 			StringUtil.equalsIgnoreCase(portalDomain, _LOCALHOST) ||
@@ -7150,12 +7150,6 @@ public class PortalImpl implements Portal {
 		}
 
 		return portalDomain.substring(0, pos);
-	}
-
-	protected String getCanonicalDomain(
-		String virtualHostname, String portalDomain) {
-
-		return getCanonicalDomain(true, virtualHostname, portalDomain);
 	}
 
 	protected Map<String, List<Portlet>> getCategoriesMap(
