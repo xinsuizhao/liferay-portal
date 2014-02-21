f75799798b4b00ffad93453269489ede82af0940
Manuel de la Peña <manuel.delapenya@liferay.com>
2014-02-21 09:06:18 +0100

    LPS-44143 SF

--- portal-impl/src/com/liferay/portal/search/lucene/LuceneHelperImpl.java
+++ portal-impl/src/com/liferay/portal/search/lucene/LuceneHelperImpl.java
@@ -428,6 +428,13 @@ public class LuceneHelperImpl implements LuceneHelper {
 	}
 
 	@Override
+	public IndexSearcher getIndexSearcher(long companyId) throws IOException {
+		IndexAccessor indexAccessor = getIndexAccessor(companyId);
+
+		return indexAccessor.acquireIndexSearcher();
+	}
+
+	@Override
 	public long getLastGeneration(long companyId) {
 		if (!isLoadIndexFromClusterEnabled()) {
 			return IndexAccessor.DEFAULT_LAST_GENERATION;
@@ -521,13 +528,6 @@ public class LuceneHelperImpl implements LuceneHelper {
 		}
 
 		return queryTerms;
-	}
-
-	@Override
-	public IndexSearcher getIndexSearcher(long companyId) throws IOException {
-		IndexAccessor indexAccessor = getIndexAccessor(companyId);
-
-		return indexAccessor.acquireIndexSearcher();
 	}
 
 	/**
@@ -662,7 +662,8 @@ public class LuceneHelperImpl implements LuceneHelper {
 	}
 
 	@Override
-	public void releaseIndexSearcher(long companyId, IndexSearcher indexSearcher)
+	public void releaseIndexSearcher(
+			long companyId, IndexSearcher indexSearcher)
 		throws IOException {
 
 		IndexAccessor indexAccessor = getIndexAccessor(companyId);
