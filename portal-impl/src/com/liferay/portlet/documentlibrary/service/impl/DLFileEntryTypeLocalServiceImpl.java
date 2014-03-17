a32c42a08e283f40a893e6c3195e16bc40ce7927
Preston-Crary <preston.crary@liferay.com>
2014-03-17 10:26:57 -0700

    LPS-45172 SF

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryTypeLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryTypeLocalServiceImpl.java
@@ -672,7 +672,7 @@ public class DLFileEntryTypeLocalServiceImpl
 			if (ddmStructure == null) {
 				throw new NoSuchMetadataSetException(
 					"No structure exists with ddmStructureId " +
-					ddmStructureId);
+						ddmStructureId);
 			}
 		}
 	}
