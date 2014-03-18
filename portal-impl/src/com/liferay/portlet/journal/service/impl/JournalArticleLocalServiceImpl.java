96c815dcb6b118a8d131c915e99ecce234067bb7
Tibor Lipusz <tibor.lipusz@liferay.com>
2014-03-18 11:21:02 +0100

    LPS-45164 Sort

--- portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -4820,6 +4820,8 @@ public class JournalArticleLocalServiceImpl
 			user, groupId, articleId, article.getVersion(), addNewVersion,
 			content, ddmStructureKey, images);
 
+		article.setUserId(user.getUserId());
+		article.setUserName(user.getFullName());
 		article.setModifiedDate(serviceContext.getModifiedDate(now));
 		article.setFolderId(folderId);
 		article.setTreePath(article.buildTreePath());
@@ -4839,8 +4841,6 @@ public class JournalArticleLocalServiceImpl
 		article.setReviewDate(reviewDate);
 		article.setIndexable(indexable);
 		article.setSmallImage(smallImage);
-		article.setUserId(user.getUserId());
-		article.setUserName(user.getFullName());
 
 		if (smallImage) {
 			if ((smallImageFile != null) && (smallImageBytes != null)) {
