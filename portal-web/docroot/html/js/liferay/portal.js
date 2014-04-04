166274fddfc0bc5003eff7a2d9794ed85058ecab
Brian Chiu <brian.chiu@liferay.com>
2014-04-04 11:35:32 -0700

    Revert "LPS-39619 - Source formatting"
    This reverts commit 5a78cd84acd8e6335b18f1b1f8ea0914e1fd86c1.

--- portal-web/docroot/html/js/liferay/portal.js
+++ portal-web/docroot/html/js/liferay/portal.js
@@ -138,11 +138,9 @@
 
 			cached.get(TRIGGER).detach('hover');
 
-			cached.get(TRIGGER).on(
-				'hover',
+			cached.get(TRIGGER).on('hover', 
 				A.bind(cached._onBoundingBoxMouseenter, cached),
-				A.bind(cached._onBoundingBoxMouseleave, cached)
-			);
+				A.bind(cached._onBoundingBoxMouseleave, cached));
 
 			cached.show();
 		},
