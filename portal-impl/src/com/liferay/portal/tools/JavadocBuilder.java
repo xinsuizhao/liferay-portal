66a35ee755e66a97421e731d6704982dbda0978b
Brian Chan <brian.chan@liferay.com>
2014-04-04 21:57:05 -0700

    Update copyright date, use -present

--- portal-impl/src/com/liferay/portal/tools/JavadocBuilder.java
+++ portal-impl/src/com/liferay/portal/tools/JavadocBuilder.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
@@ -93,7 +93,7 @@ public class JavadocBuilder {
 
 		String comment = _getCDATA(javaClass);
 
-		if (comment.startsWith("Copyright (c) 2000-2010 Liferay, Inc.")) {
+		if (comment.startsWith("Copyright (c) 2000-present Liferay, Inc.")) {
 			comment = StringPool.BLANK;
 		}
 
