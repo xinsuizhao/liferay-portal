aa1eb003a98b01349ef0012e79b3d2c35d2f8259
Brian Chiu <brian.chiu@liferay.com>
2014-04-16 16:17:57 -0700

    Revert "LPS-45320 Adds permission check to parent categories"
    This reverts commit 36387d4e42a35a9a226df9b494f0f8bc4cae66f3.

--- portal-impl/src/com/liferay/portlet/asset/service/permission/AssetCategoryPermission.java
+++ portal-impl/src/com/liferay/portlet/asset/service/permission/AssetCategoryPermission.java
@@ -17,9 +17,7 @@ package com.liferay.portlet.asset.service.permission;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.security.auth.PrincipalException;
-import com.liferay.portal.security.permission.ActionKeys;
 import com.liferay.portal.security.permission.PermissionChecker;
-import com.liferay.portal.util.PropsValues;
 import com.liferay.portlet.asset.model.AssetCategory;
 import com.liferay.portlet.asset.model.AssetCategoryConstants;
 import com.liferay.portlet.asset.service.AssetCategoryLocalServiceUtil;
@@ -32,7 +30,7 @@ public class AssetCategoryPermission {
 	public static void check(
 			PermissionChecker permissionChecker, AssetCategory category,
 			String actionId)
-		throws PortalException, SystemException {
+		throws PortalException {
 
 		if (!contains(permissionChecker, category, actionId)) {
 			throw new PrincipalException();
@@ -60,41 +58,19 @@ public class AssetCategoryPermission {
 	}
 
 	public static boolean contains(
-			PermissionChecker permissionChecker, AssetCategory category,
-			String actionId)
-		throws PortalException, SystemException {
+		PermissionChecker permissionChecker, AssetCategory category,
+		String actionId) {
 
-		if (actionId.equals(ActionKeys.VIEW) &&
-			!AssetVocabularyPermission.contains(
-				permissionChecker, category.getVocabularyId(),
-				ActionKeys.VIEW)) {
+		if (permissionChecker.hasOwnerPermission(
+				category.getCompanyId(), AssetCategory.class.getName(),
+				category.getCategoryId(), category.getUserId(), actionId)) {
 
-			return false;
+			return true;
 		}
 
-		if (actionId.equals(ActionKeys.VIEW) &&
-			PropsValues.PERMISSIONS_VIEW_DYNAMIC_INHERITANCE) {
-
-			long categoryId = category.getCategoryId();
-
-			while (categoryId !=
-						AssetCategoryConstants.DEFAULT_PARENT_CATEGORY_ID) {
-
-				category = AssetCategoryLocalServiceUtil.getCategory(
-					categoryId);
-
-				if (!_hasPermission(permissionChecker, category, actionId)) {
-					return false;
-				}
-
-				categoryId = category.getParentCategoryId();
-			}
-
-			return AssetVocabularyPermission.contains(
-				permissionChecker, category.getVocabularyId(), actionId);
-		}
-
-		return _hasPermission(permissionChecker, category, actionId);
+		return permissionChecker.hasPermission(
+			category.getGroupId(), AssetCategory.class.getName(),
+			category.getCategoryId(), actionId);
 	}
 
 	public static boolean contains(
@@ -120,23 +96,6 @@ public class AssetCategoryPermission {
 			categoryId);
 
 		return contains(permissionChecker, category, actionId);
-	}
-
-	private static boolean _hasPermission(
-		PermissionChecker permissionChecker, AssetCategory category,
-		String actionId) {
-
-		if (permissionChecker.hasOwnerPermission(
-				category.getCompanyId(), AssetCategory.class.getName(),
-				category.getCategoryId(), category.getUserId(), actionId) ||
-				permissionChecker.hasPermission(
-					category.getGroupId(), AssetCategory.class.getName(),
-					category.getCategoryId(), actionId)) {
-
-			return true;
-		}
-
-		return false;
 	}
 
 }
