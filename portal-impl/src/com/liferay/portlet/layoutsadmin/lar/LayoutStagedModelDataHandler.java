c92b92f462e6d53d9920300aa417d9fdf217d118
arbetts <andrew.betts@liferay.com>
2014-05-05 11:40:06 -0700

    LPS-44803 Unrelated SF - use the same constants all over the SMDH

--- portal-impl/src/com/liferay/portlet/layoutsadmin/lar/LayoutStagedModelDataHandler.java
+++ portal-impl/src/com/liferay/portlet/layoutsadmin/lar/LayoutStagedModelDataHandler.java
@@ -221,7 +221,7 @@ public class LayoutStagedModelDataHandler
 			(Map<Long, Layout>)portletDataContext.getNewPrimaryKeysMap(
 				Layout.class + ".layout");
 
-		String action = layoutElement.attributeValue("action");
+		String action = layoutElement.attributeValue(Constants.ACTION);
 
 		if (action.equals(Constants.DELETE)) {
 			Layout deletingLayout =
