c1bfe676fb4049353581bef0d5401779edff98ce
Miguel Angelo <miguel.angelo@liferay.com>
2014-05-30 16:25:52 -0300

    LPS-45702 Infinite loop when updating web content with sub structure

--- portal-impl/src/com/liferay/portlet/dynamicdatamapping/model/impl/DDMStructureImpl.java
+++ portal-impl/src/com/liferay/portlet/dynamicdatamapping/model/impl/DDMStructureImpl.java
@@ -466,7 +466,7 @@ public class DDMStructureImpl extends DDMStructureBaseImpl {
 
 		boolean hasField = fieldsMap.containsKey(fieldName);
 
-		while (!hasField && (getParentStructureId() > 0)) {
+		if (!hasField && (getParentStructureId() > 0)) {
 			DDMStructure parentStructure =
 				DDMStructureLocalServiceUtil.getStructure(
 					getParentStructureId());
