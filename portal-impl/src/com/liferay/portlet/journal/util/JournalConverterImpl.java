ed603ac58e87c06e8489406375b47d1d203767ba
Dustin Ryerson <dustin.ryerson@liferay.com>
2014-06-13 11:28:12 -0700

    LPS-45706 Spaces in web content structure's selection list's values displayed as %20 after upgrade to 6.2

--- portal-impl/src/com/liferay/portlet/journal/util/JournalConverterImpl.java
+++ portal-impl/src/com/liferay/portlet/journal/util/JournalConverterImpl.java
@@ -24,6 +24,7 @@ import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.CharPool;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HtmlUtil;
+import com.liferay.portal.kernel.util.HttpUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
@@ -273,11 +274,12 @@ public class JournalConverterImpl implements JournalConverter {
 			if ((parentType != null) && parentType.equals("select")) {
 				metadataElement.addAttribute("locale", defaultLanguageId);
 
-				addMetadataEntry(metadataElement, "label", name);
+				addMetadataEntry(
+					metadataElement, "label", HttpUtil.decodeURL(name));
 
 				element.addAttribute("name", "option" + StringUtil.randomId());
 				element.addAttribute("type", "option");
-				element.addAttribute("value", type);
+				element.addAttribute("value", HttpUtil.decodeURL(type));
 
 				return;
 			}
