74333dec269511bb8f8d08ff098dc21af13fd972
Kwang Lee <kwang.lee@liferay.com>
2014-06-30 16:16:43 -0700

    LPS-48135 - Resizing browser window in POSHI

--- portal-web/test/functional/com/liferay/portalweb/portal/util/liferayselenium/LiferaySelenium.java
+++ portal-web/test/functional/com/liferay/portalweb/portal/util/liferayselenium/LiferaySelenium.java
@@ -191,6 +191,8 @@ public interface LiferaySelenium extends Selenium {
 
 	public void replyToEmail(String to, String body) throws Exception;
 
+	public void resizeWindow(String value) throws Exception;
+
 	public void saveScreenshot() throws Exception;
 
 	public void saveScreenshotAndSource() throws Exception;
