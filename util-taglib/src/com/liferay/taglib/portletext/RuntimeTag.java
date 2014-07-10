f5de8dfb125d99acb7f855f83a2cb0eb00366a8b
Brian Chiu <brian.chiu@liferay.com>
2014-07-10 14:32:00 -0700

    Revert "LPS-47984 move duplicate methods into Util class"
    This reverts commit 6e8a4f2edafe758d47ccd95397fb62a8577619b0.

--- util-taglib/src/com/liferay/taglib/portletext/RuntimeTag.java
+++ util-taglib/src/com/liferay/taglib/portletext/RuntimeTag.java
@@ -14,6 +14,7 @@
 
 package com.liferay.taglib.portletext;
 
+import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.log.Log;
@@ -25,6 +26,7 @@ import com.liferay.portal.kernel.portlet.PortletParameterUtil;
 import com.liferay.portal.kernel.portlet.RestrictPortletServletRequest;
 import com.liferay.portal.kernel.servlet.DynamicServletRequest;
 import com.liferay.portal.kernel.servlet.PipingServletResponse;
+import com.liferay.portal.kernel.util.HtmlUtil;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.model.Layout;
@@ -35,6 +37,9 @@ import com.liferay.portal.service.PortletPreferencesLocalServiceUtil;
 import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portal.util.PortletKeys;
 import com.liferay.portlet.PortletPreferencesFactoryUtil;
+
+import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -134,13 +139,13 @@ public class RuntimeTag extends TagSupport {
 			}
 
 			if (jsonObject != null) {
-				PortletJSONUtil.writeHeaderPaths(response, jsonObject);
+				writeHeaderPaths(response, jsonObject);
 			}
 
 			PortletContainerUtil.render(request, response, portlet);
 
 			if (jsonObject != null) {
-				PortletJSONUtil.writeFooterPaths(response, jsonObject);
+				writeFooterPaths(response, jsonObject);
 			}
 		}
 		finally {
@@ -208,6 +213,74 @@ public class RuntimeTag extends TagSupport {
 		portlet.setStatic(true);
 
 		return portlet;
+	}
+
+	protected static void writeFooterPaths(
+			HttpServletResponse response, JSONObject jsonObject)
+		throws IOException {
+
+		JSONArray footerCssPathsJSONArray = jsonObject.getJSONArray(
+			"footerCssPaths");
+		JSONArray footerJavaScriptPathsJSONArray = jsonObject.getJSONArray(
+			"footerJavaScriptPaths");
+
+		if ((footerCssPathsJSONArray.length() == 0) &&
+			(footerJavaScriptPathsJSONArray.length() == 0)) {
+
+			return;
+		}
+
+		PrintWriter printWriter = response.getWriter();
+
+		for (int i = 0; i < footerCssPathsJSONArray.length(); i++) {
+			String value = footerCssPathsJSONArray.getString(i);
+
+			printWriter.print("<link href=\"");
+			printWriter.print(HtmlUtil.escape(value));
+			printWriter.println("\" rel=\"stylesheet\" type=\"text/css\" />");
+		}
+
+		for (int i = 0; i < footerJavaScriptPathsJSONArray.length(); i++) {
+			String value = footerJavaScriptPathsJSONArray.getString(i);
+
+			printWriter.print("<script src=\"");
+			printWriter.print(HtmlUtil.escape(value));
+			printWriter.println("\" type=\"text/javascript\"></script>");
+		}
+	}
+
+	protected static void writeHeaderPaths(
+			HttpServletResponse response, JSONObject jsonObject)
+		throws IOException {
+
+		JSONArray headerCssPathsJSONArray = jsonObject.getJSONArray(
+			"headerCssPaths");
+		JSONArray headerJavaScriptPathsJSONArray = jsonObject.getJSONArray(
+			"headerJavaScriptPaths");
+
+		if ((headerCssPathsJSONArray.length() == 0) &&
+			(headerJavaScriptPathsJSONArray.length() == 0)) {
+
+			return;
+		}
+
+		PrintWriter printWriter = response.getWriter();
+
+		for (int i = 0; i < headerCssPathsJSONArray.length(); i++) {
+			String value = headerCssPathsJSONArray.getString(i);
+
+			printWriter.print("<link href=\"");
+			printWriter.print(HtmlUtil.escape(value));
+			printWriter.println("\" rel=\"stylesheet\" type=\"text/css\" />");
+		}
+
+		for (int i = 0; i < headerJavaScriptPathsJSONArray.length(); i++) {
+			String value = headerJavaScriptPathsJSONArray.getString(i);
+
+			printWriter.print("<script src=\"");
+			printWriter.print(HtmlUtil.escape(value));
+			printWriter.println("\" type=\"text/javascript\"></script>");
+		}
 	}
 
 	private static Log _log = LogFactoryUtil.getLog(RuntimeTag.class);
