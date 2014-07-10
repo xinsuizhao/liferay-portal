da6ca9863033bb4cf3173a2dc892831216c38541
michael.bradford@liferay.com <michael.bradford@liferay.com>
2014-07-10 10:33:47 -0700

    LPS-48342 - After a page is created, it cannot be deleted immediately; the page must be refreshed first

--- portal-web/docroot/html/js/liferay/navigation.js
+++ portal-web/docroot/html/js/liferay/navigation.js
@@ -394,7 +394,7 @@ AUI.add(
 							}
 						}
 						else {
-							listItem.addClass('lfr-nav-sortable lfr-nav-updateable sortable-item');
+							listItem.addClass('lfr-nav-deletable lfr-nav-sortable lfr-nav-updateable sortable-item');
 
 							instance._createDeleteButton(listItem);
 
