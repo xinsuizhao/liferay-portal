66f76a23b81bcf3b5e2cb9204de49e18a1fc560b
IstvanD <istvan.dezsi@liferay.com>
2014-07-17 16:18:26 +0200

    LPS-48617 Search fails with ArrayIndexOutOfBoundsException

--- portal-impl/src/com/liferay/portlet/journalcontentsearch/util/ContentHits.java
+++ portal-impl/src/com/liferay/portlet/journalcontentsearch/util/ContentHits.java
@@ -43,7 +43,7 @@ public class ContentHits {
 		List<Document> docs = new ArrayList<Document>();
 		List<Float> scores = new ArrayList<Float>();
 
-		for (int i = 0; i < hits.getLength(); i++) {
+		for (int i = 0; i < hits.getDocs().length; i++) {
 			Document doc = hits.doc(i);
 
 			long articleGroupId = GetterUtil.getLong(doc.get(Field.GROUP_ID));
