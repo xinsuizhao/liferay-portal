liferay/liferay-central-test-1

LPS-46756 Sort by usage

portal-impl/src/com/liferay/portlet/asset/service/impl/AssetEntryLocalServiceImpl.java
@@ -422,6 +422,10 @@ public class AssetEntryLocalServiceImpl extends AssetEntryLocalServiceBaseImpl {
 		throws SystemException {
 
 		try {
+			Indexer indexer = AssetSearcher.getInstance();
+
+			AssetSearcher assetSearcher = (AssetSearcher)indexer;
+
 			AssetEntryQuery assetEntryQuery = new AssetEntryQuery();
 
 			assetEntryQuery.setClassNameIds(
@@ -453,10 +457,6 @@ public class AssetEntryLocalServiceImpl extends AssetEntryLocalServiceBaseImpl {
 
 			queryConfig.setHighlightEnabled(false);
 			queryConfig.setScoreEnabled(false);
-
-			Indexer searcher = AssetSearcher.getInstance();
-
-			AssetSearcher assetSearcher = (AssetSearcher)searcher;
 
 			assetSearcher.setAssetEntryQuery(assetEntryQuery);
 

xinsuizhao/liferay-portal

portal-impl/src/com/liferay/portlet/asset/service/impl/AssetEntryLocalServiceImpl.java
@@ -464,6 +464,10 @@ public class AssetEntryLocalServiceImpl extends AssetEntryLocalServiceBaseImpl {
 		throws SystemException {
 
 		try {
+			Indexer searcher = AssetSearcher.getInstance();
+
+			AssetSearcher assetSearcher = (AssetSearcher)searcher;
+
 			AssetEntryQuery assetEntryQuery = new AssetEntryQuery();
 
 			assetEntryQuery.setClassNameIds(
@@ -488,10 +492,6 @@ public class AssetEntryLocalServiceImpl extends AssetEntryLocalServiceBaseImpl {
 
 			queryConfig.setHighlightEnabled(false);
 			queryConfig.setScoreEnabled(false);
-
-			Indexer searcher = AssetSearcher.getInstance();
-
-			AssetSearcher assetSearcher = (AssetSearcher)searcher;
 
 			assetSearcher.setAssetEntryQuery(assetEntryQuery);
 