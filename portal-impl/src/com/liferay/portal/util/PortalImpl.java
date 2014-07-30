0b48b50331926e9506c059c29a73fe9743116c24
Brian Chan <brian.chan@liferay.com>
2014-07-30 10:42:25 -0700

    LPS-48834 SF

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -7469,7 +7469,7 @@ public class PortalImpl implements Portal {
 
 		String portalURL = themeDisplay.getPortalURL();
 
-		if (useGroupVirtualHostName(
+		if (isUseGroupVirtualHostName(
 				group, privateLayoutSet, themeDisplay, canonicalURL)) {
 
 			String virtualHostname = layoutSet.getVirtualHostname();
@@ -7922,7 +7922,7 @@ public class PortalImpl implements Portal {
 		themeDisplay.setLocale(locale);
 	}
 
-	protected boolean useGroupVirtualHostName(
+	protected boolean isUseGroupVirtualHostName(
 		Group group, boolean privateLayoutSet, ThemeDisplay themeDisplay,
 		boolean canonicalURL) throws SystemException {
 
