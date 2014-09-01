d3c23ed06e9ae4c01c918295d05d5eb3e1e1de14
Tamas Molnar <tamas.molnar@liferay.com>
2014-09-01 11:30:30 +0200

    LPS-49761 Use the groupId instead of the companyId to prevent DuplicateTagExceptions

--- portal-impl/src/com/liferay/portlet/wiki/importers/mediawiki/MediaWikiImporter.java
+++ portal-impl/src/com/liferay/portlet/wiki/importers/mediawiki/MediaWikiImporter.java
@@ -526,7 +526,7 @@ public class MediaWikiImporter implements WikiImporter {
 
 				try {
 					assetTag = AssetTagLocalServiceUtil.getTag(
-						node.getCompanyId(), categoryName);
+						node.getGroupId(), categoryName);
 				}
 				catch (NoSuchTagException nste) {
 					ServiceContext serviceContext = new ServiceContext();
