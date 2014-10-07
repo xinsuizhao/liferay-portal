9b87eba386af6df0e1c87f27fc05819925d86506
Zsolt Szabo <zsolt.szabo@liferay.com>
2014-10-07 11:44:44 +0200

    LPS-50622 Time.getRelativeTimeDescription uses hard-coded formatter to provide Date string

--- portal-service/src/com/liferay/portal/kernel/util/Time.java
+++ portal-service/src/com/liferay/portal/kernel/util/Time.java
@@ -175,7 +175,8 @@ public class Time {
 	}
 
 	public static String getRelativeTimeDescription(
-		long milliseconds, Locale locale, TimeZone timeZone) {
+		long milliseconds, Format dateTimeFormat, Locale locale,
+		TimeZone timeZone) {
 
 		Format timeFormat = FastDateFormatFactoryUtil.getTime(locale, timeZone);
 
@@ -205,10 +206,18 @@ public class Time {
 				locale, "yesterday-at-x", timeFormat.format(milliseconds));
 		}
 
-		Format dateFormat = FastDateFormatFactoryUtil.getSimpleDateFormat(
-			"EEEE, MMMMM dd, yyyy", locale, timeZone);
+		if (dateTimeFormat == null) {
+			dateTimeFormat = FastDateFormatFactoryUtil.getSimpleDateFormat(
+				"EEEE, MMMMM dd, yyyy", locale, timeZone);
+		}
 
-		return dateFormat.format(milliseconds);
+		return dateTimeFormat.format(milliseconds);
+	}
+
+	public static String getRelativeTimeDescription(
+		long milliseconds, Locale locale, TimeZone timeZone) {
+
+		return getRelativeTimeDescription(milliseconds, null, locale, timeZone);
 	}
 
 	public static String getRFC822() {
