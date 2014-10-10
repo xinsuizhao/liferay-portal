7898cb3c89c7c5160665e6a3e9b71f9f80e9b543
adolfo.perez <adolfo.perez@liferay.com>
2014-10-10 08:43:58 +0200

    LPS-50700 remove XML declaration from diff
    An XML declaration is always added to the output diff. As the diff is
    never a valid XML document and it will be always embedded as an HTML
    fragment, we should remove the decl line.

--- portal-impl/src/com/liferay/portal/util/DiffHtmlImpl.java
+++ portal-impl/src/com/liferay/portal/util/DiffHtmlImpl.java
@@ -117,7 +117,15 @@ public class DiffHtmlImpl implements DiffHtml {
 
 		unsyncStringWriter.flush();
 
-		return unsyncStringWriter.toString();
+		String diff = unsyncStringWriter.toString();
+
+		if (diff.startsWith("<?xml")) {
+			int i = diff.indexOf("?>", 2);
+
+			diff = diff.substring(i + 2);
+		}
+
+		return diff;
 	}
 
 	private static final String _DIFF = "diff";
