ae79328e8afbbc7454e603991e79ba3b3d2ca44d
Brian Chan <brian.chan@liferay.com>
2014-10-10 10:19:31 -0700

    LPS-50700 SF

--- portal-impl/src/com/liferay/portal/util/DiffHtmlImpl.java
+++ portal-impl/src/com/liferay/portal/util/DiffHtmlImpl.java
@@ -117,15 +117,15 @@ public class DiffHtmlImpl implements DiffHtml {
 
 		unsyncStringWriter.flush();
 
-		String diff = unsyncStringWriter.toString();
+		String string = unsyncStringWriter.toString();
 
-		if (diff.startsWith("<?xml")) {
-			int i = diff.indexOf("?>", 2);
+		if (string.startsWith("<?xml")) {
+			int index = string.indexOf("?>");
 
-			diff = diff.substring(i + 2);
+			string = string.substring(index + 2);
 		}
 
-		return diff;
+		return string;
 	}
 
 	private static final String _DIFF = "diff";
