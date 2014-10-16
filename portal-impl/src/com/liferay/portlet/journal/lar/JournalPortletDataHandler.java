19a031d6248840c3e7e9714137467aaa3265977f
Jose A. Jimenez <jose.jimenez@liferay.com>
2014-10-16 16:28:47 +0200

    LPS-50925 add order to the export

--- portal-impl/src/com/liferay/portlet/journal/lar/JournalPortletDataHandler.java
+++ portal-impl/src/com/liferay/portlet/journal/lar/JournalPortletDataHandler.java
@@ -16,6 +16,7 @@ package com.liferay.portlet.journal.lar;
 
 import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
 import com.liferay.portal.kernel.dao.orm.DynamicQuery;
+import com.liferay.portal.kernel.dao.orm.OrderFactoryUtil;
 import com.liferay.portal.kernel.dao.orm.Property;
 import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
@@ -325,6 +326,13 @@ public class JournalPortletDataHandler extends BasePortletDataHandler {
 
 		return new JournalArticleExportActionableDynamicQuery(
 			portletDataContext) {
+
+			@Override
+			protected void addCriteria(DynamicQuery dynamicQuery) {
+				super.addCriteria(dynamicQuery);
+
+				dynamicQuery.addOrder(OrderFactoryUtil.asc("id"));
+			}
 
 			@Override
 			protected void performAction(Object object) throws PortalException {
