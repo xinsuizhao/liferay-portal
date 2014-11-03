31164daa2ef0b6c312383a5d16b16f83181ed2aa
jgok <joshua.gok@liferay.com>
2014-11-03 09:17:10 -0800

    LPS-50910 fix logic: we should only return a url to a layoutset if there are pages in that layoutset

--- portal-impl/src/com/liferay/portal/model/impl/GroupImpl.java
+++ portal-impl/src/com/liferay/portal/model/impl/GroupImpl.java
@@ -179,9 +179,9 @@ public class GroupImpl extends GroupBaseImpl {
 
 		String portalURL = themeDisplay.getPortalURL();
 
-		int publicLayoutsPageCount = group.getPublicLayoutsPageCount();
+		if ((privateLayout && (getPrivateLayoutsPageCount() > 0)) ||
+			(!privateLayout && (getPublicLayoutsPageCount() > 0))) {
 
-		if (publicLayoutsPageCount > 0) {
 			StringBundler sb = new StringBundler(5);
 
 			sb.append(portalURL);
