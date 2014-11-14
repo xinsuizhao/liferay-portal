f938b1d0c1f63f3e421a8319d7eb82d299e8e6b2
Zoltán Takács <zoltan.takacs@liferay.com>
2014-11-14 12:27:57 +0100

    LPS-50878 SF

--- portal-impl/src/com/liferay/portal/lar/ExportImportHelperImpl.java
+++ portal-impl/src/com/liferay/portal/lar/ExportImportHelperImpl.java
@@ -121,7 +121,6 @@ import com.liferay.portlet.dynamicdatamapping.model.DDMStructure;
 import com.liferay.portlet.dynamicdatamapping.model.DDMTemplate;
 import com.liferay.portlet.dynamicdatamapping.service.DDMStructureLocalServiceUtil;
 import com.liferay.portlet.dynamicdatamapping.service.persistence.DDMStructureUtil;
-import com.liferay.portlet.journal.lar.JournalContentPortletDataHandler;
 import com.liferay.portlet.journal.model.JournalArticle;
 
 import java.io.File;
